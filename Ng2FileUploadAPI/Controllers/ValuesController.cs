0a5e6f61ffff948bb9c5ef0a171651cad4263a56
Jatin Prajapati <jatin_s_prajapati@yahoo.com>
2018-03-01 15:17:04 -0500

    Initial commit

---
+++ Ng2FileUploadAPI/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using System.Threading;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Ng2FileUploadAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private IHostingEnvironment _env;
+        public ValuesController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPost]
+        [Route("upload")]
+        public async Task<IActionResult> PostFile()
+        {
+            var files = Request.Form.Files;
+
+            foreach (var file in files)
+            {
+                var filename = ContentDispositionHeaderValue
+                              .Parse(file.ContentDisposition)
+                              .FileName
+                              .Trim('"');
+              
+                var savePath = Path.Combine(_env.ContentRootPath, "uploads", filename);
+
+                using (var fileStream = new FileStream(savePath, FileMode.Create))
+                {
+                    await file.OpenReadStream().CopyToAsync(fileStream);
+                }
+                return Created(savePath, file);
+            }
+            return Ok();
+        }
+
+
+
+    }
+
+}
---
+++ Ng2FileUploadAPI/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Ng2FileUploadAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+        }
+        private const string PolicyName = "CorsPolicy";
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddCors(options => options.AddPolicy(PolicyName, build =>
+            {
+                build
+                    .SetPreflightMaxAge(TimeSpan.MaxValue)
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin()
+                    .AllowCredentials()
+                    .AllowAnyHeader();
+            }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors(PolicyName);
+            app.UseMvc();
+        }
+    }
+}
